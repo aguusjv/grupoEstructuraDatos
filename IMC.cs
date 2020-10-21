732be109cbf61b7a5a7cfbd7a4fc3c201368b104
Cristian <cristianozor@gmail.com>
2020-10-20 23:30:43 -0300

    Hay que arreglar el archivo IMC

---
+++ IMC.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC
+{
+    class Class1
+    {
+        string categoriaIMC(string sexoTipo, double numero)
+        {
+            string tipoCategoria = "";
+            if (sexoTipo == "F")
+            {
+                if (numero < 20)
+                {
+                    tipoCategoria = "BAJO PESO";
+                }
+                else if (numero >= 20 && numero <= 23.9)
+                {
+                    tipoCategoria = "NORMAL";
+                }
+                else if (numero >= 24 && numero <= 28.9)
+                {
+                    tipoCategoria = "OBESIDAD LEVE";
+                }
+                else if (numero >= 29 && numero <= 37)
+                {
+                    tipoCategoria = "OBESIDAD SEVERA";
+                }
+                else
+                {
+                    tipoCategoria = "OBESIDAD MUY SEVERA";
+                }
+            }
+            if (sexoTipo == "M")
+            {
+                if (numero < 20)
+                {
+                    tipoCategoria = "BAJO PESO";
+                }
+                else if (numero >= 20 && numero <= 24.9)
+                {
+                    tipoCategoria = "NORMAL";
+                }
+                else if (numero >= 25 && numero <= 29.9)
+                {
+                    tipoCategoria = "OBESIDAD LEVE";
+                }
+                else if (numero >= 30 && numero <= 40)
+                {
+                    tipoCategoria = "OBESIDAD SEVERA";
+                }
+                else
+                {
+                    tipoCategoria = "OBESIDAD MUY SEVERA";
+                }
+
+            }
+            return tipoCategoria;
+
+        }
+        categoriaIMC("F"; 24);
+    }
+}
---
+++ Principal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double indiceIMC(double pesoFuncion, int alturaFuncion)
+            {
+                double conversionAltura = (Convert.ToDouble(alturaFuncion))/100;
+                double potenciaMasaCorp = Math.Pow(conversionAltura, 2);
+                double resultadoIMC = pesoFuncion / potenciaMasaCorp;
+                return resultadoIMC;
+
+            }
+
+            
+            Console.WriteLine("Ingrese su altura en centímetros");
+            int altura = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su peso en kilogramos");
+            double peso = double.Parse(Console.ReadLine());
+
+            double calculoIMC = indiceIMC(peso, altura);
+            Console.WriteLine("Su IMC es de {0}", calculoIMC);
+            Console.ReadKey();
+        }
+    }
+}
